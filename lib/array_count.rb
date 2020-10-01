83aeaeec55dd0fca3f68fd8aae08b359a349d83d
Vivek Sanghvi <viksipart@gmail.com>
2020-10-01 16:09:49 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,7 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   array.count do |ele|
-    ele.class = String 
+    ele.class == String 
   end
       
 end
@@ -9,4 +9,6 @@ end
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
   array.count do |ele|
+    ele == ""
+  end
 end
